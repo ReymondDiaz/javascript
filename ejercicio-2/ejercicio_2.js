65d237d77d4ba85d7a2bafe08dbfc1f35e657af7
reymond Diaz <reymonddiaz959@gmail.com>
2023-03-30 22:58:21 -0500

    curso javascript

---
+++ ejercicio-2/ejercicio_2.js
@@ -0,0 +1,12 @@
+const lista = [
+  "Reymond",
+  29,
+  true,
+  new Date(1994, 10, 27),
+  { 
+      titulo: "Don Quijote De La Mancha",
+      autor: "Miguel De Cervantes",
+      fecha: new Date(1605, 0, 16),
+      url: "https://www.amazon.com/-/es/Miguel-Cervantes-ebook/dp/B01GT03OP2/ref=sr_1_1?crid=2M74RLI2XSNU5&keywords=don+quijote+de+la+mancha+in+spanish+original&qid=1680227710&sprefix=don+q%2Caps%2C293&sr=8-1"
+    },
+];
