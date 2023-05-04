542005848f9264882f6941532748bfeff9c6a7ef
reymond Diaz <reymonddiaz959@gmail.com>
2023-05-03 20:58:23 -0500

    agregando ejercicios

--- ejercicio-3/ejercicio-3.js
+++ ejercicio-3/ejercicio-3.js
@@ -14,6 +14,12 @@ for(let i = 1; i<10; i++){
 }
 console.log(factorial);
 
+let factorial5 = 1
+for (let k = 10; k > 0; k--) {
+    factorial5 *= k
+}
+console.log(factorial5) 
+
 let factorial2= 10;
 let i = factorial2 - 1;
 while(i > 1){
@@ -21,6 +27,14 @@ while(i > 1){
     i--;
 }
 console.log(factorial2);
+
+let factorial6 = 1
+let num1 = 10
+while (num1 > 1) {
+    factorial6 *= num1;
+    num1--;
+}
+console.log(factorial6) 
 
 let factorial3 = 10;
 let j = factorial3 - 1;
@@ -30,3 +44,12 @@ while (factorial3){
     if ( j<= 1) break;
 }
 console.log(factorial3);
+
+let factorial4 = 1
+let num = 10
+while (true) {
+    factorial4 *= num
+    num--
+    if (num === 1) break
+}
+console.log(factorial4) 
