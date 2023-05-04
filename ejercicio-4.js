5dcd5c8af0a1bc449d86d23c2177384ccd4bbaa6
reymond Diaz <reymonddiaz959@gmail.com>
2023-05-03 23:57:39 -0500

    ejercicio 4

---
+++ ejercicio-4.js
@@ -0,0 +1,52 @@
+/*Crea un archivo JS que contenga las siguientes líneas
+
+- Una cadena de texto con tu Nombre
+
+- Otra cadena de texto con tu Apellido
+
+- Una cadena de texto que se llame "estudiante" concatenando tu Nombre y tu Apellido con un espacio entre medias
+
+- Una cadena de texto que se llame "estudianteMayus" que contenga la cadena estudiante pero todo en mayúsculas
+
+- Una cadena de texto que se llame "estudianteMinus" que contenga la cadena estudiante pero todo en minúsculas
+
+- Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios
+
+- Una variable que contenga la primera letra del Nombre
+
+- Otra variable que contenga la última letra del Apellido
+
+- Una cadena de texto que elimine los espacios de la variable "estudiante"
+
+- Una variable booleana que diga si el Nombre está contenido en la variable "estudiante"*/
+
+let nombre = " Reymond ";
+console.log(nombre);
+
+let apellido = "Diaz ";
+console.log(apellido);
+
+let estudiante = nombre + " " +  apellido;
+console.log(estudiante);
+
+let estudianteMayus = estudiante.toUpperCase();
+console.log(estudianteMayus);
+
+let estudianteMinus = estudiante.toLowerCase();
+console.log(estudianteMinus);
+
+let numLetras = estudiante.length;
+console.log(numLetras);
+
+let primerLetra = nombre.charAt(0);
+console.log(primerLetra);
+
+let ultimaLetra = apellido.charAt(3);
+console.log(ultimaLetra);
+
+let eliminarEspacios = estudiante.trim();
+console.log(eliminarEspacios);
+
+let verificarNombre = estudiante.includes("Reymond")
+console.log(verificarNombre);
+
